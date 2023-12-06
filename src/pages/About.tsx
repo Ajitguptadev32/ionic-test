d0a99f4c306eefe96826a6b05f5342bfc1fd1c70
Ajitguptadev32 <dev32@bytescrum.com>
2023-12-06 17:52:59 +0530

    first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab3.css';
+
+const about: React.FC = () => {
+  return (
+    <IonPage>
+   <h1>About</h1>
+    </IonPage>
+  );
+};
+
+export default about;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,21 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import React from 'react';
+
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photos</IonTitle>
+          
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonCard color="success">
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        <IonButton fill="clear">Action 1</IonButton>
+      <IonButton fill="clear">Action 2</IonButton>
+      </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+        
         <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>
